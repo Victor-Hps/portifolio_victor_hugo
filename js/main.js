e12fd1ff1be11bc0f1bb6674f969d291bf6893ef
Victor-Hps <99192151+Victor-Hps@users.noreply.github.com>
2024-01-02 18:31:05 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const   navMenu = document.getElementById('nav-menu'),
+        navToggle = document.getElementById('nav-toggle'),
+        navClose = document.getElementById('nav-close')
+
+if(navToggle){
+   navToggle.addEventListener('click',()=>{
+     navMenu.classList.add('show-menu')
+       })
+}
+
+if(navClose){
+   navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+ })
+}
+
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 5 ? header.classList.add('shadow-header')
+                : header.classList.remove('shadow-header')
+}
+
+window,addEventListener('scroll', shadowHeader)
